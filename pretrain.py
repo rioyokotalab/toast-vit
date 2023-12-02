facd6c4858fd93674810069e822463e1c0de8fb6
riverstone496 <robosatoki@outlook.jp>
2023-12-02 17:09:04 +0900

    fix dates

--- pretrain.py
+++ pretrain.py
@@ -658,10 +658,9 @@ def main():
     output_dir = None
     if args.rank == 0:
         exp_name = '-'.join([
-            datetime.now().strftime("%Y%m%d-%H%M%S"),
+            datetime.now().strftime("%Y%m%d-%H%M%S.%f"),
             safe_model_name(args.model),
-            str(data_config['input_size'][-1]),
-            str(rand.randint(10000, 99999))
+            str(data_config['input_size'][-1])
         ])
         output_dir = get_outdir(args.output if args.output else './output/train', exp_name)
         decreasing = True if eval_metric == 'loss' else False
